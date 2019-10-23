6294768b4d192d44b427d0bea3d25198730cfaa7
CesarCedanoV <CesarCedanoV@gmail.com>
2019-10-23 01:28:58 -0400

    Updated the main file to load the GoogleMaps and also show a new screen to add new places (markers)

--- maps_geolocation/lib/main.dart
+++ maps_geolocation/lib/main.dart
@@ -1,57 +1,120 @@
+import 'dart:async';
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:maps_geolocation/new_sight_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: MapSample(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  GoogleMapController _mapController;
+  List<Marker> _markers = [];
+
+  static final CameraPosition _kAgoraMall = CameraPosition(
+    target: LatLng(18.4835421, -69.9398128),
+    zoom: 17,
+  );
+
+  static final CameraPosition _kBlueMall = CameraPosition(
+      bearing: 45.8334901395799,
+      target: LatLng(18.47258932432269, -69.94110390543938),
+      tilt: 9.440717697143555,
+      zoom: 17.151926040649414);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return new Scaffold(
       appBar: AppBar(
         title: Text('Maps with Geolocation'),
-        backgroundColor: Colors.amber,
+        backgroundColor: Colors.blue,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            MaterialButton(
-              child: Text('Login with Google'),
-              color: Colors.white,
-              textColor: Colors.black,
-              onPressed: () {},
-            ),
-            MaterialButton(
-              child: Text('Logout'),
-              color: Colors.red,
-              textColor: Colors.black,
-              onPressed: () {},
-            )
-          ],
-        ),
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            initialCameraPosition: _kAgoraMall,
+            mapType: MapType.normal,
+            onMapCreated: _onMapCreated,
+            onLongPress: _newSight,
+            markers: Set<Marker>.of(_markers),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheBlueMall,
+        label: Text('Take me to Blue Mall'),
+        icon: Icon(Icons.local_mall),
       ),
     );
+  }
+
+  void _newSight(LatLng position) {
+    print(position);
+    Navigator.of(context)
+        .push(
+      MaterialPageRoute(builder: (ctx) => NewSightScreen()),
+    )
+        .then((markerOpts) {
+      if (markerOpts != null) {
+        _addMarker(position, markerOpts);
+        _mapController.animateCamera(
+          CameraUpdate.newCameraPosition(
+            CameraPosition(
+              target: position,
+              zoom: 17,
+            ),
+          ),
+        );
+      } else {
+        print(markerOpts);
+      }
+    });
+  }
+
+  _addMarker(LatLng position, Map<String, String> markerOpts) {
+    List<Marker> tempMarkers = _markers;
+    tempMarkers.add(
+      Marker(
+        markerId: MarkerId(DateTime.now().toString()),
+        position: position,
+        icon: BitmapDescriptor.defaultMarker,
+        infoWindow: InfoWindow(
+            title: markerOpts['title'], snippet: markerOpts['snippet']),
+        draggable: true,
+        // onTap: () {},
+      ),
+    );
+
+    setState(() {
+      _markers = tempMarkers;
+    });
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      _mapController = controller;
+    });
+  }
+
+  Future<void> _goToTheBlueMall() async {
+    // final GoogleMapController controller = await _mapController.future;
+    _mapController.animateCamera(CameraUpdate.newCameraPosition(_kBlueMall));
   }
 }
---
+++ maps_geolocation/lib/new_sight_screen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class NewSightScreen extends StatelessWidget {
+  final TextEditingController _title = TextEditingController();
+  final TextEditingController _snippet = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          // title: Text('Create a new Place!'),
+          leading: IconButton(
+            icon: Icon(Icons.close),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              stops: [0, 1],
+              colors: [
+                Colors.greenAccent,
+                Colors.cyanAccent,
+              ],
+            ),
+          ),
+          child: Center(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Color.fromRGBO(255, 255, 255, 0.95),
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(width: 1)),
+              padding: const EdgeInsets.all(10),
+              height: 250,
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Create a new Place! ',
+                    style: TextStyle(fontSize: 25, color: Colors.blueGrey),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    controller: _title,
+                    maxLength: 60,
+                    decoration: InputDecoration(
+                      icon: Icon(Icons.pin_drop),
+                      hintText: 'Enter the sight title',
+                    ),
+                  ),
+                  TextField(
+                    controller: _snippet,
+                    maxLength: 250,
+                    decoration: InputDecoration(
+                      icon: Icon(Icons.description),
+                      hintText: 'Enter a description',
+                    ),
+                  ),
+                  FlatButton(
+                    child: Text('Create'),
+                    onPressed: () {
+                      Navigator.of(context).pop({
+                        'title': _title.text,
+                        'snippet': _snippet.text,
+                      });
+                    },
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
